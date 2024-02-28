21e1afac18c8c7ee7ec66746d5d785c7c6345404
Swati jagtap <swatijagtap5060@gmail.com>
2024-02-28 14:52:26 +0530

    Assignment submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-function App(){
-  return(
+import React from 'react'
 
+const App = () => {
+  return (
     <>
-    <div id="main>
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <div 
+    id="main">
+       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
     </>
-  );
+  )
 }
-export default App;
+
+export default App
+
